91dc2d9d39921fa8354a5051e5ac57b51d8f0c04
unknown <mickael.croquet@laposte.net>
2020-05-05 15:27:46 +0200

    Adding toggle menu for navbar

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Assets/lspd11.png";
 import { LinkList } from "./LinkList";
 
-const Header = () => (
-  <div className="flex flex-wrap w-full bg-blue-800 p-2 z-20">
-    <div className="text-gray-700 text-center mx-auto p-2">
-      <img alt="lspdLogo" className="m-auto" src={Logo}></img>
-      <hr className="my-4" />
-      <h1 className="text-2xl text-white">LSPD BBCODE GENERATOR</h1>
-    </div>
-    <nav className="flex w-full items-center justify-between flex-wrap  p-6">
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow text-center">
-          <LinkList />
-        </div>
+const Header = () => {
+  const [isCollapse, setIsCollapse] = useState(false);
+  const ToggleIsCollapse = () => setIsCollapse(!isCollapse);
+  return (
+    <div className="z-20 flex flex-wrap w-full p-2 bg-blue-800">
+      <div className="p-2 mx-auto text-center text-gray-700">
+        <img alt="lspdLogo" className="m-auto" src={Logo}></img>
+        <hr className="my-4" />
+        <h1 className="text-2xl text-white">LSPD BBCODE GENERATOR</h1>
       </div>
-    </nav>
-  </div>
-);
+      <nav className="flex flex-wrap items-center justify-between w-full p-6">
+        <div className="block lg:hidden">
+          <button
+            className="flex items-center px-3 py-2 text-gray-200 border border-gray-400 rounded hover:text-white hover:border-white"
+            onClick={ToggleIsCollapse}
+          >
+            <svg
+              className="w-3 h-3 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className={`flex-grow block w-full lg:flex lg:items-center lg:w-auto ${isCollapse ? "block" : "hidden"}`}>
+          <div className="text-sm text-center lg:flex-grow">
+            <LinkList />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export { Header };
